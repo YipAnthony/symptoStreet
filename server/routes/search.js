2566932c284a7759b610a77219db311d2f12c0f6
AnthonyLegionLaptop <anthonyjtyip@gmail.com>
2020-12-17 17:27:38 -0800

    Reformat server-side search functions

--- server/routes/search.js
+++ server/routes/search.js
@@ -33,58 +33,58 @@ router.post('/', [
     const searchQuery = {}
 
     // Add the following paramaters to search query if input by user
-    if (Number(req.body.zipcode) !== 0) {
-      searchQuery.zipcode = req.body.zipcode
+      if (Number(req.body.zipcode) !== 0) {
+        searchQuery.zipcode = req.body.zipcode
+        
+        // Generate zipcodes within an x mile radius
+        const zipcodesWithinRadius = zipcodes.radius(req.body.zipcode, req.body.zipcodeRadius) 
+
+        if (zipcodesWithinRadius.length > 0) {
+          searchQuery.zipcode = {$in: zipcodesWithinRadius}
+        }
+      } 
+
+      const userBedFilter = req.body.bedsInput
+      if (userBedFilter !== "") {
+        if (userBedFilter === "4") {
+          searchQuery.beds = {"$gte": 4}
+        } else {
+          searchQuery.beds = {"$gte": userBedFilter[0], "$lte": userBedFilter[1]}
+        }
+      }
       
-      // Generate zipcodes within an x mile radius
-      const zipcodesWithinRadius = zipcodes.radius(req.body.zipcode, req.body.zipcodeRadius) 
-
-      if (zipcodesWithinRadius.length > 0) {
-        searchQuery.zipcode = {$in: zipcodesWithinRadius}
+      const userBathFilter = req.body.bathsInput
+      if (userBathFilter !== "") {
+        if (userBathFilter === "4") {
+          searchQuery.baths = {"$gte": 4}
+        } else {
+          searchQuery.baths = {"$gte": userBathFilter[0], "$lte": userBathFilter[1]}
+        }
       }
-    } 
-
-    const userBedFilter = req.body.bedsInput
-    if (userBedFilter !== "") {
-      if (userBedFilter === "4") {
-        searchQuery.beds = {"$gte": 4}
-      } else {
-        searchQuery.beds = {"$gte": userBedFilter[0], "$lte": userBedFilter[1]}
+      
+      if (!!req.body.sqftInputMin) {
+        searchQuery.sqft = {}
+        searchQuery.sqft["$gte"] = req.body.sqftInputMin
       }
-    }
-    
-    const userBathFilter = req.body.bathsInput
-    if (userBathFilter !== "") {
-      if (userBathFilter === "4") {
-        searchQuery.baths = {"$gte": 4}
-      } else {
-        searchQuery.baths = {"$gte": userBathFilter[0], "$lte": userBathFilter[1]}
+      if (!!req.body.sqftInputMax) {
+        if (!searchQuery.sqft) searchQuery.sqft = {}
+        searchQuery.sqft["$lte"] = req.body.sqftInputMax
       }
-    }
-    
-    if (!!req.body.sqftInputMin) {
-      searchQuery.sqft = {}
-      searchQuery.sqft["$gte"] = req.body.sqftInputMin
-    }
-    if (!!req.body.sqftInputMax) {
-      if (!searchQuery.sqft) searchQuery.sqft = {}
-      searchQuery.sqft["$lte"] = req.body.sqftInputMax
-    }
 
-    if(!!req.body.priceInputMin) {
-      searchQuery.price = {}
-      searchQuery.price["gte"] = req.body.priceInputMin
-    }
-    if(!!req.body.priceInputMax) {
-      if (!searchQuery.price) searchQuery.price = {}
-      searchQuery.price["gte"] = req.body.priceInputMax
-    }
+      if(!!req.body.priceInputMin) {
+        searchQuery.price = {}
+        searchQuery.price["gte"] = req.body.priceInputMin
+      }
+      if(!!req.body.priceInputMax) {
+        if (!searchQuery.price) searchQuery.price = {}
+        searchQuery.price["gte"] = req.body.priceInputMax
+      }
 
-    if (req.body.address !== "") searchQuery.address = {"$regex":req.body.address, "$options": "i"} 
+      if (req.body.address !== "") searchQuery.address = {"$regex":req.body.address, "$options": "i"} 
     
     // Search MongoDB with user inputted query
-    const searchOutput = await House.find(searchQuery).sort([['price', req.body.priceSort]])
-    console.log(searchQuery)
+    const searchOutput = await House.find(searchQuery)
+      .sort([['price', req.body.priceSort]]) //sort results based on price
     res.json(searchOutput)
   }
 ]);
