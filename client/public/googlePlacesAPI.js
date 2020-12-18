c24b1e2ea2a110cbebbbb9b420c8b2f4903bd2fb
AnthonyLegionLaptop <anthonyjtyip@gmail.com>
2020-12-17 17:31:07 -0800

    Add comments to googleAPI file

--- client/public/googlePlacesAPI.js
+++ client/public/googlePlacesAPI.js
@@ -1,3 +1,4 @@
+// Create google address search API on initial page render
 let autocomplete;
 function initAutocomplete() {
   autocomplete = new google.maps.places.Autocomplete(
@@ -8,7 +9,6 @@ function initAutocomplete() {
       fields: ['place_id', 'geometry', 'name']
     }
   )
-
   autocomplete.addListener('place_changed', onPlaceChanged)
 }
 
@@ -18,7 +18,8 @@ function onPlaceChanged() {
   if (!place.geometry) {
     document.getElementById('googleAddress').placeholder = 'Search an address'
   } else {
-    
+    // The API didn't integrate well with React, so I ended up having to assign longitude/latitude data
+    // to custom attributes to retrieve later 
     document.getElementById('googleAddress').setAttribute('latitude', place.geometry.location.lat()) 
     document.getElementById('googleAddress').setAttribute('longitude', place.geometry.location.lng())  
     document.getElementById('googleAddress').setAttribute('resultName', place.name)  
